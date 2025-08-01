df3354f0a25871679350beeab280b4caea3bcb5c
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-07-31 22:34:09 -0500

    Refactorizo los endpond para un mejor rendimiento.
    Añado el endpoind de actualización de productos y refactorizar consultas
    de base de datos.
    
    Los cambios amplían la API con la funcionalidad PUT para actualizar
    productos y mejoran la estructura general del código con consultas SQL
    más eficientes y un mejor manejo de errores.

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,94 @@
 import Database from "bun:sqlite";
 import swagger from "@elysiajs/swagger";
-import { Elysia, t } from "elysia";
+import { Elysia, status, t } from "elysia";
 
-const dbFileName = "./src/data/ease_tec.db";
-const db = new Database(dbFileName);
+const rutaDeBaseDeDatos = "./src/data/ease_tec.db";
+const db = new Database(rutaDeBaseDeDatos);
 
 const app = new Elysia()
   .use(swagger())
 
+  .get("/", () => "Api para los productos de Eese-Tec")
+
+  .get("/categoria", () => {
+    try {
+      const solicitudDeCategorias = db.query("SELECT * FROM categorias;");
+
+      const categorias = solicitudDeCategorias.all();
+
+      return categorias;
+    } catch (error) {
+      console.error(`Error al obtener las categoría: ${error}`);
+
+      return status(500, "Error interno del servidor");
+    }
+  })
+
   .get("/producto", () => {
     try {
-      const query = db.query("SELECT * FROM productos;");
+      const solicitudDeProductos = db.query(`
+        SELECT
+          id_producto,
+          nombre_producto,
+          url_imagen,
+          precio,
+          stock,
+          nombre_categoria
+        FROM
+          productos AS A
+            JOIN
+          categorias AS B
+            ON A.id_categoria = B.id_categoria;
+        `);
 
-      const productos = query.all();
+      const productos = solicitudDeProductos.all();
 
       return productos;
     } catch (error) {
       console.error(`Error al obtener al producto: ${error}`);
 
-      return new Response("Error interno del servidor", { status: 500 });
+      return status(500, "Error interno del servidor");
     }
   })
 
   .post(
     "/producto",
     ({
-      body: { nombre, descripcion, precio_venta, stock_actual, id_categoria },
+      body: {
+        nuevo_nombre,
+        nuevo_precio_venta,
+        nuevo_stock_actual,
+        nuevo_id_categoria,
+      },
     }) => {
       try {
-        const query = db.query(`
+        const solicitudDeNuevoProducto = db.query(`
         INSERT INTO productos
-          (nombre, descripcion, precio_venta, stock_actual, id_categoria)
+          (nombre_producto, precio, stock, id_categoria)
         VALUES
-          ($nombre, $descripcion, $precio_venta, $stock_actual, $id_categoria);
+          ($nombre_producto, $precio, $stock, $id_categoria);
       `);
 
-        query.run({
-          $nombre: nombre,
-          $descripcion: descripcion ?? "Sin descripción",
-          $precio_venta: precio_venta,
-          $stock_actual: stock_actual,
-          $id_categoria: id_categoria,
+        solicitudDeNuevoProducto.run({
+          $nombre_producto: nuevo_nombre,
+          $precio: nuevo_precio_venta,
+          $stock: nuevo_stock_actual,
+          $id_categoria: nuevo_id_categoria,
         });
 
         return "Agregado correctamente";
       } catch (error) {
         console.error(`Error al agregar: ${error}`);
 
-        return new Response("Error interno del servidor", { status: 500 });
+        return status(500, "Error interno del servidor");
       }
     },
     {
       body: t.Object({
-        nombre: t.String(),
-        descripcion: t.Optional(t.String()),
-        precio_venta: t.Numeric(),
-        stock_actual: t.Integer(),
-        id_categoria: t.Integer(),
+        nuevo_nombre: t.String(),
+        nuevo_precio_venta: t.Numeric(),
+        nuevo_stock_actual: t.Integer(),
+        nuevo_id_categoria: t.Integer(),
       }),
     },
   )
@@ -65,17 +97,25 @@ const app = new Elysia()
     "/producto/:id",
     ({ params: { id } }) => {
       try {
-        const query = db.query(
-          "SELECT nombre FROM productos WHERE id_producto = $id_producto;",
-        );
+        const solicitudDeProductoPorId = db.query(`
+          SELECT
+           id_producto ,nombre_producto, url_imagen, precio, stock, nombre_categoria
+          FROM
+            productos AS A
+              JOIN
+            categorias AS B
+              ON A.id_categoria = B.id_categoria
+          WHERE
+            A.id_producto = ?;
+        `);
 
-        const producto = query.get({ $id_producto: id });
+        const producto = solicitudDeProductoPorId.get(id);
 
         return producto;
       } catch (error) {
         console.error(`Error al obtener al producto: ${error}`);
 
-        return new Response("Error interno del servidor", { status: 500 });
+        return status(500, "Error interno del servidor");
       }
     },
     {
@@ -83,8 +123,105 @@ const app = new Elysia()
     },
   )
 
+  .put(
+    "/producto/:id",
+    ({
+      params: { id },
+      body: {
+        id_categoria,
+        nombre_producto,
+        precio_venta,
+        stock_actual,
+        url_imagen,
+      },
+    }) => {
+      try {
+        const productoParaActualizar = db
+          .query(
+            "SELECT id_producto, id_categoria FROM productos WHERE id_producto = ?;",
+          )
+          .get(id) as { id_producto: number; id_categoria: number };
+
+        if (productoParaActualizar.id_producto >= 25) {
+          return "Llegaste al límite. Solicita más recurso a tu Programador";
+        }
+
+        if (!productoParaActualizar) {
+          return status(404, "Producto no encontrado");
+        }
+
+        if (id_categoria !== undefined) {
+          const categoriaParaActualizar = db
+            .query(
+              "SELECT id_categoria FROM categorias WHERE id_categoria = ?;",
+            )
+            .get(id_categoria);
+
+          if (!categoriaParaActualizar) {
+            return status(400, "Categoría no encontrada");
+          }
+        }
+
+        const camposParaActualizar: string[] = [];
+        const valorParaAgregar: (number | string)[] = [];
+
+        if (nombre_producto !== undefined) {
+          camposParaActualizar.push("nombre_producto = $nombre");
+          valorParaAgregar.push(nombre_producto);
+        }
+        if (url_imagen !== undefined) {
+          camposParaActualizar.push("url_imagen = $url_imagen");
+          valorParaAgregar.push(url_imagen);
+        }
+        if (precio_venta !== undefined) {
+          camposParaActualizar.push("precio = $precio");
+          valorParaAgregar.push(precio_venta);
+        }
+        if (stock_actual !== undefined) {
+          camposParaActualizar.push("stock = $stock");
+          valorParaAgregar.push(stock_actual);
+        }
+        if (id_categoria !== undefined) {
+          camposParaActualizar.push("id_categoria = $id_categoria");
+          valorParaAgregar.push(id_categoria);
+        }
+
+        if (camposParaActualizar.length === 0) {
+          return status(400, "No hay datos para actualizar");
+        }
+
+        valorParaAgregar.push(id);
+
+        const putQuery = db.query(`
+          UPDATE producto_productos
+          SET ${camposParaActualizar.join(", ")}
+          WHERE id_producto = ?;
+        `);
+
+        putQuery.run(...valorParaAgregar);
+
+        return "Actualizado con éxito";
+      } catch (error) {
+        console.error(`Error al actualizar producto: ${error}`);
+
+        return status(505, "Error del servidor");
+      }
+    },
+    {
+      params: t.Object({ id: t.Integer() }),
+      body: t.Object({
+        nombre_producto: t.Optional(t.String()),
+        descripcion: t.Optional(t.String()),
+        url_imagen: t.Optional(t.String()),
+        precio_venta: t.Optional(t.Numeric()),
+        stock_actual: t.Optional(t.Integer()),
+        id_categoria: t.Optional(t.Integer()),
+      }),
+    },
+  )
+
   .listen(process.env.PORT ?? 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia esta corriendo en el puerto ${app.server?.hostname}:${app.server?.port}`,
 );
